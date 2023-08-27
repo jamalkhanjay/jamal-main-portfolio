49c6865bc44414ea9c1bd3ed171191b6b3bb77d4
jamalkhanwazir <jamalkhanwaxir@gmail.com>
2023-08-27 08:14:27 +0500

    fixed some padding errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState} from "react";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header, { HeaderPhone } from "./components/Header";
@@ -8,12 +8,12 @@ import Timeline from "./components/Timeline";
 import Work from "./components/Work";
 
 function App() {
-
   const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
-      <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Header menuOpen = {menuOpen} setMenuOpen = {setMenuOpen} />
+      <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Home />
       <Work />
       <Timeline />
